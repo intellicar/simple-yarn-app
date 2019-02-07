943452e143208731a7e6a88c8fdce3b5109967ac
vinodbaba <vinodn@intellicar.in>
2019-02-07 16:20:21 +0530

    inc changes

---
+++ src/main/java/com/hortonworks/simpleyarnapp/MyApp.java
@@ -0,0 +1,18 @@
+package com.hortonworks.simpleyarnapp;
+
+public class MyApp {
+
+
+    public static void main(String[] args) {
+
+        int n = 0;
+        while( n < 60){
+            try {
+                Thread.sleep(1000);
+            }
+            catch (Exception e){
+                
+            }
+        }
+    }
+}
